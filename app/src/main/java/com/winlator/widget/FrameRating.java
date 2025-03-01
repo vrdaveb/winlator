f77ec3444a5c2f7ca513d4ee6cdc63d23cac22af
Pipetto-crypto <Pipetto-crypto@pipetto-crypto.com>
2025-03-01 17:01:11 +0100

    FrameRating: Add a CPU, GPU and RAM information to FrameRating

--- app/src/main/java/com/winlator/widget/FrameRating.java
+++ app/src/main/java/com/winlator/widget/FrameRating.java
@@ -1,5 +1,6 @@
 package com.winlator.widget;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.os.SystemClock;
 import android.util.AttributeSet;
@@ -12,17 +13,29 @@ import android.widget.TextView;
 import com.winlator.R;
 
 import com.winlator.core.FileUtils;
+import com.winlator.core.StringUtils;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Locale;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FrameRating extends FrameLayout implements Runnable {
+    private Context context;
     private long lastTime = 0;
     private int frameCount = 0;
     private File appInfo = null;
     private float lastFPS = 0;
     private String renderer = null;
+    private String gpuName = null;
+    private String totalRAM = null;
     private final TextView tvFPS;
     private final TextView tvRenderer;
+    private final TextView tvCPU;
+    private final TextView tvGPU;
+    private final TextView tvRAM;
 
     public FrameRating(Context context) {
         this(context, null);
@@ -34,12 +47,85 @@ public class FrameRating extends FrameLayout implements Runnable {
 
     public FrameRating(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
+        this.context = context;
         View view = LayoutInflater.from(context).inflate(R.layout.frame_rating, this, false);
         tvFPS = view.findViewById(R.id.TVFPS);
         tvRenderer = view.findViewById(R.id.TVRenderer);
+        tvCPU = view.findViewById(R.id.TVCPU);
+        tvCPU.setText(getSOCName(context));
+        tvGPU = view.findViewById(R.id.TVGPU);
+        tvRAM = view.findViewById(R.id.TVRAM);
+        totalRAM = getTotalRAM();
         appInfo = new File(context.getFilesDir(), "imagefs/tmp/app_info.txt");
+        if (appInfo.exists()) appInfo.delete();
         addView(view);
+    }
+    
+    private String getRenderer() {
+        String renderer = "";
+        ArrayList<String> lines = FileUtils.readLines(appInfo);
+        if (lines.size() > 0)
+            renderer = lines.get(0);
+        return renderer;
+    }
+    
+    private String getGPUName() {
+        String gpuName = "";
+        ArrayList<String> lines = FileUtils.readLines(appInfo);
+        if (lines.size() > 1) {
+            gpuName = lines.get(1);
+        }
+        return gpuName;
+    }
+
+    private String getBoardName() {
+        String boardName = "";
+        try {
+            boardName = (String)Class.forName("android.os.SystemProperties").getMethod("get", String.class).invoke(null, "ro.board.platform");
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return boardName;
+    }
+    
+    private String getSOCName(Context context) {
+        String socName = "";
+        InputStream is = null;
+
+        try {
+            is = context.getAssets().open("cpu_database.json");
+            if (is != null) {
+                String jsonString = new String(is.readAllBytes());
+                JSONObject jobj = new JSONObject(jsonString);
+                JSONObject board = (JSONObject)jobj.get(getBoardName());
+                socName = board.getString("SoC");
+            }
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+        
+        return socName;
+    }
+    
+    private String getTotalRAM() {
+        String totalRAM = "";
+        ActivityManager activityManager = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+        totalRAM = StringUtils.formatBytes(memoryInfo.totalMem);
+        return totalRAM;
+    }
+    
+    private String getAvailableRAM() {
+        String availableRAM = "";
+        ActivityManager activityManager = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+        long usedMem = memoryInfo.totalMem - memoryInfo.availMem;
+        availableRAM = StringUtils.formatBytes(usedMem, false);
+        return availableRAM;
     }
     
     public void reset() {
@@ -51,7 +137,9 @@ public class FrameRating extends FrameLayout implements Runnable {
 
     public void update() {
         if (lastTime == 0) lastTime = SystemClock.elapsedRealtime();
-        if (appInfo.exists() && renderer == null) renderer = new String(FileUtils.read(appInfo));
+        if (appInfo.exists() && renderer == null) renderer = getRenderer();
+        if (appInfo.exists() && gpuName == null) gpuName = getGPUName();
+            
         long time = SystemClock.elapsedRealtime();
         if (time >= lastTime + 500) {
             lastFPS = ((float)(frameCount * 1000) / (time - lastTime));
@@ -67,6 +155,8 @@ public class FrameRating extends FrameLayout implements Runnable {
     public void run() {
         if (getVisibility() == GONE) setVisibility(View.VISIBLE);
         tvFPS.setText(String.format(Locale.ENGLISH, "%.1f", lastFPS));
-        tvRenderer.setText(String.format(Locale.ENGLISH, "%s", renderer));
+        if (!tvRenderer.getText().toString().equals(renderer)) tvRenderer.setText(renderer);
+        if (!tvGPU.getText().toString().equals(gpuName)) tvGPU.setText(gpuName);
+        tvRAM.setText(getAvailableRAM() + " GB Used / " + totalRAM + " Total");
     }
 }
