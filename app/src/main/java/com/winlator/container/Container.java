8923b8dc4820850f2996c382f27f93fe40915b79
pipetto-crypto <pipetto-crypto@gmail.com>
2025-05-14 18:03:13 +0200

    Container: set mediafoundation default status to builtin

--- app/src/main/java/com/winlator/container/Container.java
+++ app/src/main/java/com/winlator/container/Container.java
@@ -30,7 +30,7 @@ public class Container {
     public static final String DEFAULT_AUDIO_DRIVER = XrActivity.isSupported() ? "pulseaudio" : "alsa";
     public static final String DEFAULT_EMULATOR = "FEXCore";
     public static final String DEFAULT_DXWRAPPER = "dxvk";
-    public static final String DEFAULT_WINCOMPONENTS = "direct3d=1,directsound=1,directmusic=0,directshow=0,directplay=0,xaudio=0,vcrun2010=1,windowsmediafoundation=1";
+    public static final String DEFAULT_WINCOMPONENTS = "direct3d=1,directsound=1,directmusic=0,directshow=0,directplay=0,xaudio=0,vcrun2010=1,windowsmediafoundation=0";
     public static final String FALLBACK_WINCOMPONENTS = "direct3d=1,directsound=1,directmusic=1,directshow=1,directplay=1,xaudio=1,vcrun2010=1,windowsmediafoundation=1";
     public static final String DEFAULT_DRIVES = "D:"+Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"E:/data/data/com.winlator/storage";
     public static final byte STARTUP_SELECTION_NORMAL = 0;
