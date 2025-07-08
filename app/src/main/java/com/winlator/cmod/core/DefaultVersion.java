6f46fcd4f741c2b772b701bdc7b96b849dbc0e7c
pipetto-crypto <pipetto-crypto@gmail.com>
2025-07-08 18:22:52 +0200

    assets: add fexcore 2507 and make it default

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -3,7 +3,7 @@ package com.winlator.cmod.core;
 public abstract class DefaultVersion {
     public static final String BOX86 = "0.3.2";
     public static final String BOX64 = "0.3.7";
-    public static final String FEXCORE = "2505";
+    public static final String FEXCORE = "2507";
     public static final String WRAPPER = "System";
     public static final String DXVK = "1.10.1";
     public static final String D8VK = "1.0";
